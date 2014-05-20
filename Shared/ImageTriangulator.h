8c34fc6b609550be116fd84cb4b3a95fc57ef0af
Brett Renfer <brett@robotconscience.com>
2014-05-20 18:08:57 -0400

    Updated shared src

--- Shared/ImageTriangulator.h
+++ Shared/ImageTriangulator.h
@@ -6,9 +6,135 @@
 //
 //
 
-#ifndef Streaming_Triangulate_ImageTriangulator_h
-#define Streaming_Triangulate_ImageTriangulator_h
+#pragma once
 
+#include "ofMain.h"
+#include "ofxCv.h"
+#include "ofxDelaunay.h"
 
-
-#endif
+class ImageTriangulator : public ofThread {
+public:
+    
+    ImageTriangulator() :
+    threshold(.05){
+        
+    }
+    
+    void process( ofImage & image ){
+        toProcess.setFromPixels(image.getPixelsRef());
+        toProcess.setUseTexture(false);
+        bNeedToProcess = true;
+        if (!isThreadRunning()) startThread();
+    }
+    
+    virtual void reprocess(){
+        bNeedToProcess = true;
+    }
+    
+    void draw(){
+        lock();
+        mesh.draw();
+        unlock();
+    }
+    
+    virtual void threadedFunction(){
+        while(isThreadRunning()){
+            if ( bNeedToProcess ) triangulate(toProcess);
+            bNeedToProcess = false;
+            sleep(100);
+        }
+    }
+    
+    float threshold;
+    
+protected:
+    
+    ofImage toProcess;
+    ofxDelaunay triangulator;
+    ofMesh mesh;
+    bool bNeedToProcess = true;
+    
+    void triangulate( ofImage & image ){
+        triangulator.reset();
+        vector<ofPoint> points = findPoints( image, threshold / 1000 );
+        //triangulator.addPoints( points );
+        // is this still an issue?
+        triangulator.addPoint(0,0,0);
+        triangulator.addPoint(image.width,0,0);
+        triangulator.addPoint(image.width,image.height,0);
+        triangulator.addPoint(0,image.height,0);
+        for ( auto p : points ){
+            // why do we have to do this ? :(
+            float x = ofRandom(p.x,p.x+.1), y =  ofRandom(p.y,p.y+.1);
+            triangulator.addPoint(x,y, 0 );
+        }
+        triangulator.triangulate();
+        
+        mesh.clear();
+        
+        ofColor white;
+        for ( auto v : triangulator.triangleMesh.getVertices() ){
+            triangulator.triangleMesh.addColor(white);
+        }
+        
+        // set colors
+        for ( int i=0, len = triangulator.triangleMesh.getNumIndices(); i<len; i+=3){
+            ofIndexType i1 = triangulator.triangleMesh.getIndex( (ofIndexType) i);
+            ofIndexType i2 = triangulator.triangleMesh.getIndex( (ofIndexType) i+1);
+            ofIndexType i3 = triangulator.triangleMesh.getIndex( (ofIndexType) i+2);
+            
+            ofPoint p1 = triangulator.triangleMesh.getVertex( i1 );
+            ofPoint p2 = triangulator.triangleMesh.getVertex( i2 );
+            ofPoint p3 = triangulator.triangleMesh.getVertex( i3 );
+            
+            ofPoint center;
+            center.x = (p1.x + p2.x + p3.x )/3.0;
+            center.y = (p1.y + p2.y + p3.y )/3.0;
+            
+            ofColor c = image.getColor(round(center.x), round(center.y));
+            //        c.a = 200;
+            ofFloatColor toSet; toSet.set( c.r/255.0f, c.g/255.0f, c.b/255.0f, 1.0f);
+            triangulator.triangleMesh.setColor(i1, toSet);
+            triangulator.triangleMesh.setColor(i2, toSet);
+            triangulator.triangleMesh.setColor(i3, toSet);
+            
+            mesh.addIndex(mesh.getNumVertices());
+            mesh.addVertex(p1);
+            mesh.addColor(toSet);
+            mesh.addIndex(mesh.getNumVertices());
+            mesh.addVertex(p2);
+            mesh.addColor(toSet);
+            mesh.addIndex(mesh.getNumVertices());
+            mesh.addVertex(p3);
+            mesh.addColor(toSet);
+        }
+    }
+    
+    vector<ofPoint> findPoints( ofImage & image, float threshold ){
+        cv::Mat toProcess = ofxCv::toCv(image);
+        vector<cv::KeyPoint> objectKeypoints;
+        vector<ofPoint> points;
+        
+        // Extract key points
+        //    The detector can be any of (see OpenCV features2d.hpp):
+        //    ﻿cv::FeatureDetector * detector = new cv::DenseFeatureDetector();
+        //    ﻿cv::FeatureDetector * detector = new cv::FastFeatureDetector();
+        //    ﻿cv::FeatureDetector * detector = new cv::GFTTDetector();
+        //    cv::FeatureDetector * detector = new cv::MSER();
+        //    ﻿cv::FeatureDetector * detector = new cv::ORB();
+        //    ﻿cv::FeatureDetector * detector = new cv::StarFeatureDetector();
+        //    ﻿cv::FeatureDetector * detector = new cv::SURF(600.0);
+        const cv::SIFT::DetectorParams& detectorParams = cv::SIFT::DetectorParams(threshold, 10.0);
+        
+        cv::FeatureDetector * detector = new cv::SiftFeatureDetector( detectorParams );
+        detector->detect(toProcess, objectKeypoints);
+        delete detector;
+        
+        // convert to ofVec2f vector
+        for ( int i=0, len = objectKeypoints.size(); i<len; i++){
+            points.push_back( ofVec2f( objectKeypoints[i].pt.x, objectKeypoints[i].pt.y ) );
+        }
+        return points;
+    }
+    
+};
--- Shared/RandomTriangulator.h
+++ Shared/RandomTriangulator.h
@@ -1,14 +1,72 @@
 //
-//  RanomTriangulator.h
+//  RandomTriangulator.h
 //  Streaming_Triangulate
 //
 //  Created by Brett Renfer on 5/20/14.
 //
 //
 
-#ifndef Streaming_Triangulate_RanomTriangulator_h
-#define Streaming_Triangulate_RanomTriangulator_h
+#pragma once
 
+#include "ImageTriangulator.h"
 
-
-#endif
+class RandomTriangulator : public ImageTriangulator {
+public:
+    
+    float density;
+    
+    virtual void threadedFunction(){
+        while(isThreadRunning()){
+            if ( bNeedToProcess ){
+                triangulate(toProcess);
+                
+                for ( auto v : mesh.getVertices() ){
+                    mesh.addTexCoord(v);
+                }
+                
+                // randomize mesh
+                for ( int i=0, len = mesh.getNumIndices(); i<len; i+=3){
+                    ofIndexType i1 = mesh.getIndex( (ofIndexType) i);
+                    ofIndexType i2 = mesh.getIndex( (ofIndexType) i+1);
+                    ofIndexType i3 = mesh.getIndex( (ofIndexType) i+2);
+                    
+                    ofFloatColor toSet( mesh.getColor(i1) );
+                    //toSet.a = ofNoise( mesh.getVertex(i1).x + ofGetElapsedTimeMillis() * .001);
+                    toSet.a = (ofRandom(100) > density ? 1.0 : 0.0);
+                    mesh.setColor(i1, toSet);
+                    mesh.setColor(i2, toSet);
+                    mesh.setColor(i3, toSet);
+                    
+                    ofVec2f v1 = mesh.getVertex(i1);
+                    ofVec2f v2 = mesh.getVertex(i2);
+                    ofVec2f v3 = mesh.getVertex(i3);
+                    
+                    ofVec2f shift;
+                    
+                    shift.x = ofSignedNoise( v1.x + ofGetElapsedTimeMillis() * .001) * toProcess.width/4.0;
+                    shift.y = ofSignedNoise( v1.y + ofGetElapsedTimeMillis() * .001) * toProcess.height/4.0;
+                    
+                    v1 += shift;
+                    v2 += shift;
+                    v3 += shift;
+                    
+                    v1.x = ofClamp(v1.x,0,toProcess.width);
+                    v1.y = ofClamp(v1.y,0,toProcess.height);
+                    
+                    v2.x = ofClamp(v2.x,0,toProcess.width);
+                    v2.y = ofClamp(v2.y,0,toProcess.height);
+                    
+                    v3.x = ofClamp(v3.x,0,toProcess.width);
+                    v3.y = ofClamp(v3.y,0,toProcess.height);
+                    
+                    mesh.setTexCoord(i1,v1);
+                    mesh.setTexCoord(i2,v2);
+                    mesh.setTexCoord(i3,v3);
+                }
+            }
+            
+            bNeedToProcess = false;
+            sleep(100);
+        }
+    }
+};
